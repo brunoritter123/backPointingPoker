6e6b3d7acf3843775c4c0153a9cc75ff9b5139d8
Bruno Ritter <brunosk8123@hotmail.com>
2018-07-31 08:00:19 -0300

    fim do jogo

--- service.js
+++ service.js
@@ -4,6 +4,7 @@ let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
 let users  = []
+const votoNull = {value: null, label: null, type: null};
 const cartas = [
   {value: 1 , label: '1'  , type: 'default'},
   {value: 2 , label: '2'  , type: 'default'},
@@ -26,22 +27,39 @@ io.on('connection', (socket) => {
   });
   
   socket.on('add-voto', (carta) => {
+    let acabouJogo = true;
     users.forEach( (user) => {
       if(user.id == socket.id) {
         user.voto = carta;
       }
+      if(!user.voto.label) {
+        acabouJogo = false;
+      }
     });
 
     io.emit('get-user', users);
+    if(acabouJogo) {
+      io.emit('get-FimJogo', true);
+    }
   });
 
   socket.on('add-user', (userName) => {
-    users.push({id: socket.id, nome: userName, voto: {value: null, label: null, type: null}})
+    users.push({id: socket.id, nome: userName, voto: votoNull})
     io.emit('get-user', users);
   });
 
   socket.on('obs-cartas', () => {
     io.emit('get-cartas', cartas);
+  });
+
+  socket.on('add-FimJogo', (fimJogo) => {
+    io.emit('get-FimJogo', fimJogo);
+    if (!fimJogo) {
+      users.forEach( (user) => {
+        (user.voto = votoNull)
+      });
+      io.emit('get-user', users);
+    }
   });
 
 });
