ee51bbdd23d8cec069b454e483a735b66b8fad0c
brunoritter123 <brunosk8123@hotmail.com>
2019-04-13 17:01:36 -0300

    ajuste do banco de dados

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-module.exports = mongoose.connect('mongodb://scrumpoker:Scrumpoker12@tatooine.mongodb.umbler.com:50349/scrumpoker', { useNewUrlParser: true });
+module.exports = mongoose.connect('mongodb://scrumpoker:Scrumpoker12@kamino.mongodb.umbler.com:42852/scrumpoker', { useNewUrlParser: true });
 //module.exports = mongoose.connect('mongodb://localhost:27017/scrumpoker', { useNewUrlParser: true });
