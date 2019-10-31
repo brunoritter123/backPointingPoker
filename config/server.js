6dc928d69df3944fa305cb165435b2bbca99b585
Bruno De Lima Ritter <brunolritter123@gmail.com>
2019-10-31 13:48:33 -0300

    Integração com Jira, proxy

--- config/server.js
+++ config/server.js
@@ -2,9 +2,31 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const app = express();
+const request = require("request");
+
+
 
 const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/dist'))
+
+app.all('/api/jira/*', (req, res) => {
+    const methodRequest = req.method
+    const baseUrl = req.header('Base-Url')
+    const auth = req.header('Authorization')
+    const urlRequest = req.url.replace('/api/jira', baseUrl)
+
+    var requisicao = { method: methodRequest,
+        url: urlRequest,
+        headers:{ 'Authorization': auth } };
+
+
+    request(requisicao, function (error, response, body) {
+        if (error) throw new Error(error);
+      
+        res.send(body);
+      });
+  });
+
 app.get('/*', (req,res) => res.sendFile(path.join(__dirname)));
 const server = http.createServer(app);
 
