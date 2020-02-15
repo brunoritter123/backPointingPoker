79ef840482a1ba6c5aee694edfbd06f960ca135e
Bruno Ritter <brunolritter123@gmail.com>
2020-02-14 21:44:19 -0300

    configuração do docker

--- config/conKnex.js
+++ config/conKnex.js
@@ -1,18 +1,35 @@
 knex = require('knex');
 
-var con_string = process.env.scrumpoker_connection_string
+function createConnection() {
+  var con_string = process.env.scrumpoker_connection_string
 
-if (!con_string) {
-  con_string = {
-    client: 'sqlite3',
-    connection: {
-      filename: "./dataBase.sqlite3"
-    },
-    useNullAsDefault: true,
-    pool: { min: 0, max: 1 }
+  if (!con_string) {
+    con_string = {
+      client: 'sqlite3',
+      connection: {
+        filename: "./dataBase.sqlite3"
+      },
+      useNullAsDefault: true,
+      pool: { min: 0, max: 1 }
+    }
+  } else {
+    con_string = JSON.parse(con_string)
   }
-} else {
-  con_string = JSON.parse(con_string)
 }
+
+let tentativas = 5;
+while (tentativas) {
+  try {
+    createConnection();
+    break;
+  } catch (error) {
+    tentativas -= 1;
+    console.log(`Tentativas para conectar: ${tentativas}`);
+
+    // Aguardar 5 segundos para tentar conectar novamente
+    await new Promise(res => setTimeOut(res, 5000));
+  }
+}
+
 
 module.exports = knex(con_string)
